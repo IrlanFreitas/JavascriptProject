f9ee006c093ea97c9a053cecda45863f398afeb7
Irlan Freitas <irlan.junior@solutis.com.br>
2018-03-22 16:23:50 -0300

    Ajustes solicitados

--- programando-na-linguagem-da-web/07/js/inserirDados.js
+++ programando-na-linguagem-da-web/07/js/inserirDados.js
@@ -1,6 +1,7 @@
 function inserirPessoas(pessoas) {
 
     let pessoa2
+    let nomeQualquer = "John"
 
     for (let index = 0; index < pessoas.length; index++) {
         if (pessoas[index].lastName == "Morris") {
@@ -8,74 +9,13 @@ function inserirPessoas(pessoas) {
         }
     }
 
-    function clone(obj) {
-        if (null == obj || "object" != typeof obj) return obj;
-        // Handle Object
-        if (obj instanceof Object) {
-            copy = {};
-            for (var attr in obj) {
-                if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
-            }
-            return copy;
-        }
-
-        return copy;
-    }
-
-    console.log(pessoa2);
-
-    pessoa2._firstName = "John"
+    pessoa2._firstName = nomeQualquer
     pessoa2._avatar = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2XDFpnTyrmOzl2Is1fkSI_lXJ1ERXqpDE-93lvGsiC9uCp-r65g'
 
     pessoas.push(new Pessoa(pessoa2._id, pessoa2._firstName, pessoa2._lastName, pessoa2._avatar))
 
-    console.log(pessoas);
-
     pessoas.forEach(pessoa => {
         corpoTabela.appendChild(criarLinha(pessoa))
     });
-
-    // let aux = true
-    // for (let index = 0; index < pessoas.length; index++) {
-
-    //     if ((pessoas[index].firstName == "Charles" && pessoas[index].lastName == "Morris") && aux == true) {
-    //         aux = false
-    //     } else if (pessoas[index].lastName == "Morris" && aux == false) {
-    //         pessoas[index]._firstName = "John"
-    //     } else {
-
-    //     }
-
-    // }
-
-
-    // console.log(pessoas);
-
-
-    // let aux = true
-    // for (let index = 0; index < pessoas.length; index++) {
-
-    //     if ((pessoas[index].firstName == "Charles" && pessoas[index].lastName == "Morris") && aux == true) {
-    //         // pessoas[index]._avatar = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2XDFpnTyrmOzl2Is1fkSI_lXJ1ERXqpDE-93lvGsiC9uCp-r65g'
-    //         corpoTabela.appendChild(criarLinha(pessoas[index]))
-    //         aux = false
-    //     } else if (pessoas[index].lastName == "Morris" && aux == false) {
-    //         pessoas[index]._firstName = "John"
-    //         corpoTabela.appendChild(criarLinha(pessoas[index]))
-
-    //     } else {
-    //         corpoTabela.appendChild(criarLinha(pessoas[index]))
-    //     }
-
-    // }
-
-    // console.log(pessoas.length);
-
-    // pessoas.forEach(pessoa => {
-    //     if (pessoa.firstName == "John" && pessoa.lastName == "Morris") {
-    //         pessoa._avatar = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2XDFpnTyrmOzl2Is1fkSI_lXJ1ERXqpDE-93lvGsiC9uCp-r65g'
-    //         corpoTabela.appendChild(criarLinha(pessoa))
-    //     }
-    // })
 
 }
--- programando-na-linguagem-da-web/07/js/obter-dados.js
+++ programando-na-linguagem-da-web/07/js/obter-dados.js
@@ -1,7 +1,7 @@
 'use strict'
 
 let xhr = new XMLHttpRequest();
-let pessoas = new Array()
+
 
 xhr.open("GET", "https://reqres.in/api/users?page=2", true);
 
@@ -11,10 +11,10 @@ xhr.onload = obterDados
 
 function obterDados(dados) {
     if (dados.currentTarget.status == 200) {
+        let pessoas = new Array()
         var response = JSON.parse(dados.currentTarget.response)
 
         response.data.forEach(function(dado) {
-            console.log(dado.avatar);
             pessoas.push(new Pessoa(dado.id, dado.first_name, dado.last_name, dado.avatar))
         })
 
